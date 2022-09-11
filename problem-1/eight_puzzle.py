152caf86334791a96dac8ef498e4957d0181d721
Isariyafxk <6322773092@g.siit.tu.ac.th>
2022-09-11 18:16:37 +0700

    s
    Co-Authored-By: pipe <97576398+PIPEPIR@users.noreply.github.com>
    Co-Authored-By: Thammatouchh <113100307+Thammatouchh@users.noreply.github.com>

--- problem-1/eight_puzzle.py
+++ problem-1/eight_puzzle.py
@@ -52,7 +52,16 @@ def h1(s):
 
 
 def h2(s):
-
-
     # implement this function
+    sum = 0
+    for i in range(0,9):
+        if s[0][i] != 0:
+            R = i//3
+            C = i%3
+            RG = (s[0][i] - 1)//3
+            CG = (s[0][i] - 1)%3
+            r = abs(R - RG)
+            c = abs(C - CG)
+            N = r+c
+            sum += N
     return 0
